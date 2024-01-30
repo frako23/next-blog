0ca31b6a46c3b0ae23263b9108e949fbd11e5fcc
frako23 <frako23@gmail.com>
2024-01-29 22:01:50 -0400

    ajustes de estilos

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 export const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, lama dev here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In placeat
+            dicta iure ex dolor voluptates quam atque itaque voluptate illum cum
+            illo et, assumenda, ipsum praesentium quis repellendus vitae
+            impedit!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,9 +17,15 @@ export const Navbar = () => {
       <div className={styles.logo}>lamablog</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link className={styles.link} href="/">
+          Homepage
+        </Link>
+        <Link className={styles.link} href="/">
+          Contact
+        </Link>
+        <Link className={styles.link} href="/">
+          About
+        </Link>
         <AuthLinks />
       </div>
     </div>
