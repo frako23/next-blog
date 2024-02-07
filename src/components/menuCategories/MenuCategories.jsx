ed8c465940c364d845b63d9fa9b3297c630938ba
frako23 <frako23@gmail.com>
2024-02-07 11:17:45 -0400

    adding menu bar

---
+++ src/components/menuCategories/MenuCategories.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./menuCategories.module.css";
+
+export const MenuCategories = () => {
+  return (
+    <div className={styles.categoryList}>
+      <Link
+        href="/blog?cat=style"
+        className={`${styles.categoryItem} ${styles.style}`}
+      >
+        style
+      </Link>
+      <Link
+        href="/blog?cat=fashion"
+        className={`${styles.categoryItem} ${styles.fashion}`}
+      >
+        fashion
+      </Link>
+      <Link
+        href="/blog?cat=food"
+        className={`${styles.categoryItem} ${styles.food}`}
+      >
+        food
+      </Link>
+      <Link
+        href="/blog?cat=travel"
+        className={`${styles.categoryItem} ${styles.travel}`}
+      >
+        travel
+      </Link>
+      <Link
+        href="/blog?cat=culture"
+        className={`${styles.categoryItem} ${styles.culture}`}
+      >
+        culture
+      </Link>
+      <Link
+        href="/blog?cat=coding"
+        className={`${styles.categoryItem} ${styles.coding}`}
+      >
+        coding
+      </Link>
+    </div>
+  );
+};
