1f99651844052fc9b3d20fad7ee6adc519f3edff
frako23 <frako23@gmail.com>
2024-02-20 22:25:13 -0400

    category SSR

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,12 +2,14 @@ import { CardList } from "@/components/cardList/CardList";
 import React from "react";
 import styles from "./blogPage.module.css";
 
-export default function BlogPage() {
+export default function BlogPage({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Style Blog</h1>
+      <h1 className={styles.title}>{cat} Blog</h1>
       <div className={styles.content}>
-        <CardList />
+        <CardList page={page} cat={cat} />
       </div>
     </div>
   );
